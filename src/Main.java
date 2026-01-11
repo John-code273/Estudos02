75a843e331494e2063a5a24c09e4a0e38dd11973
john <john.silvaromao@gmail.com>
2026-01-11 16:39:50 -0300

    Versao final. Estudo de java POO. Retangulo e seus calculos.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 import java.math.*;
-void main() {
+import java.util.Scanner;
+
+public class Main{
+public static void main() {
     Object retangulo = new Object();
     Scanner sc = new Scanner(System.in);
     System.out.print("entre com a altura do retangulo: ");
     retangulo.altura = sc.nextDouble();
     System.out.print("entre com a largura do retangulo: ");
     retangulo.largura = sc.nextDouble();
-    System.out.println("Area = "+ retangulo.area());
-    System.out.println("perimetro = "+ retangulo.perimetro());
-    System.out.println("diagonal = "+ retangulo.diagonal());
+    System.out.println("Area = " + retangulo.area());
+    System.out.println("perimetro = " + retangulo.perimetro());
+    System.out.println("diagonal = " + retangulo.diagonal());
 
 
-
-
+ }
 }
